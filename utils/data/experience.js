d5b5d7c7031d2da001a9f440166c2e6e89ed33d9
anshi894 <brainway343@gmail.com>
2026-01-18 21:31:46 +0530

    Prepare project for Netlify deployment

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,20 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
+    title: 'Front-End Developer Intern',
+    company: "Prodesk IT",
+    duration: "June 2025 - July 2025"
   },
   {
     id: 2,
-    title: "FullStack Developer",
-    company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
-  },
-  {
-    id: 3,
-    title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
+    title: "Front-End Developer Intern",
+    company: "Oasis Infobyte",
+    duration: "July 2025 - August 2025"
   }
+  // {
+  //   id: 3,
+  //   title: "Self Employed",
+  //   company: "Code and build something in everyday.",
+  //   duration: "(Jan 2018 - Present)"
+  // }
 ]
